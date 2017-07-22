5a98e849970d8bf4ce82f47c671815e011e8379a
mohSeSC <mouhamed.berrimi@gmail.com>
2017-07-22 19:56:52 +0100

    Update db.php

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 
 $username = 'root';
 $password = '';
-$connection = new PDO( 'mysql:host=localhost;dbname=facultee', 
+$connection = new PDO( 'mysql:host=localhost;dbname=*your data base name here ', 
 $username, $password );
 
 ?>
